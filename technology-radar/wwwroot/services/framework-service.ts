ca865d1d163e848165ab3064ac79aa06bbfe2667
Quinntyne Brown <quinntynebrown@gmail.com>
2016-02-27 04:53:18 -0500

    implement base-service

--- technology-radar/wwwroot/services/framework-service.ts
+++ technology-radar/wwwroot/services/framework-service.ts
@@ -1,40 +1,9 @@
-﻿export class FrameworkService {
-    constructor(private $q: angular.IQService, private apiEndpoint, private fetch) { }
-    
-    get() {
-        let deferred = this.$q.defer();
-        this.fetch.fromCacheOrService({ method: "GET", url: this.baseUri + "/get" })
-            .then(results => deferred.resolve(results.data));
-        return deferred.promise;
-    };
+﻿import { BaseService } from "./base-service";
 
-    getById(options) {
-        let deferred = this.$q.defer();
-        this.fetch.fromCacheOrService({ method: "GET", url: this.baseUri + "/getById", params: { id: options.id } })
-            .then(results => deferred.resolve(results.data));
-        return deferred.promise;
-    };
-
-    add(options) {
-        let deferred = this.$q.defer();
-        this.fetch.fromService({ method: "POST", url: this.baseUri + "/add", data: options.data })
-            .then(results => deferred.resolve(results.data));
-        return deferred.promise;
-    };
-
-    update(options) {
-        let deferred = this.$q.defer();
-        this.fetch.fromService({ method: "PUT", url: this.baseUri + "/update", data: options.data })
-            .then(results => deferred.resolve(results.data));
-        return deferred.promise;
-    };
-
-    remove(options) {
-        let deferred = this.$q.defer();
-        this.fetch.fromService({ method: "DELETE", url: this.baseUri + "/remove", params: { id: options.id } })
-            .then(results => deferred.resolve(results.data));
-        return deferred.promise;
-    };
+export class FrameworkService extends BaseService {
+    constructor($q: angular.IQService, apiEndpoint, fetch) {
+        super($q, apiEndpoint, fetch)
+    }
 
     get baseUri() { return this.apiEndpoint.getBaseUrl() + "/framework"; }
 
--- technology-radar/wwwroot/services/tool-service.ts
+++ technology-radar/wwwroot/services/tool-service.ts
@@ -1,40 +1,9 @@
-﻿export class ToolService {
-    constructor(private $q: angular.IQService, private apiEndpoint, private fetch) { }
+﻿import { BaseService } from "./base-service";
 
-    get() {
-        let deferred = this.$q.defer();
-        this.fetch.fromCacheOrService({ method: "GET", url: this.baseUri + "/get" })
-            .then(results => deferred.resolve(results.data));
-        return deferred.promise;
-    };
-
-    getById(options) {
-        let deferred = this.$q.defer();
-        this.fetch.fromCacheOrService({ method: "GET", url: this.baseUri + "/getById", params: { id: options.id } })
-            .then(results => deferred.resolve(results.data));
-        return deferred.promise;
-    };
-
-    add(options) {
-        let deferred = this.$q.defer();
-        this.fetch.fromService({ method: "POST", url: this.baseUri + "/add", data: options.data })
-            .then(results => deferred.resolve(results.data));
-        return deferred.promise;
-    };
-
-    update(options) {
-        let deferred = this.$q.defer();
-        this.fetch.fromService({ method: "PUT", url: this.baseUri + "/update", data: options.data })
-            .then(results => deferred.resolve(results.data));
-        return deferred.promise;
-    };
-
-    remove(options) {
-        let deferred = this.$q.defer();
-        this.fetch.fromService({ method: "DELETE", url: this.baseUri + "/remove", params: { id: options.id } })
-            .then(results => deferred.resolve(results.data));
-        return deferred.promise;
-    };
+export class ToolService extends BaseService {
+    constructor($q: angular.IQService, apiEndpoint, fetch) {
+        super($q, apiEndpoint, fetch)
+    }
 
     get baseUri() { return this.apiEndpoint.getBaseUrl() + "/tool"; }
 
